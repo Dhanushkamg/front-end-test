8e18406e1e201ae280427b128960b16682865c4d
Dhanushkamg <dhanushkamg1234@gmail.com>
2025-05-07 15:31:12 +0530

    Project Routes Set Up

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Auth = () => {
+    return (
+        <div>
+            Auth page
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Auth;
